c7cb844f750fb419914137857673d6452e086d12
Toon Van den Bos <toon@whitecube.be>
2022-02-09 15:11:53 +0100

    Added header & footer. Added base template HTML structure

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,3 +1,10 @@
-<?php
+<?php get_header(); ?>
 
-echo "hello world";
+    <main class="layout">
+        <section class="layout__intro">
+            <h2 class="layout__title">Introduction</h2>
+            <p class="layout__txt">Bienvenue sur mon super site.</p>
+        </section>
+    </main>
+
+<?php get_footer(); ?>
