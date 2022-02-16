dad5f68ff8138213ad8e9a26a172d43640b51d70
Toon Van den Bos <toon@whitecube.be>
2022-02-16 15:21:45 +0100

    Added trips custom post type

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,4 +1,20 @@
 <?php
 
+// Désactiver l'éditeur Gutenberg de Wordpress
 add_filter('use_block_editor_for_post', '__return_false');
+
+// Activer les images pour les posts (articles, voyages, ...)
 add_theme_support('post-thumbnails');
+
+// Enregistrer un "type de ressource" (custom post type) pour les voyages
+register_post_type('trips', [
+    'label' => 'Voyages',
+    'labels' => [
+        'name' => 'Voyages',
+        'singular_name' => 'Voyage',
+    ],
+    'description' => 'La ressource permettant de gérer les voyages qui ont été effectués.',
+    'public' => true,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-palmtree',
+]);
