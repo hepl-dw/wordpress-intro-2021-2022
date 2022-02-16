e78abd0503db5edfc40d2fb3a8d5b0df5b8d2c95
Toon Van den Bos <toon@whitecube.be>
2022-02-16 15:02:11 +0100

    Added the loop

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,2 +1,4 @@
 <?php
 
+add_filter('use_block_editor_for_post', '__return_false');
+add_theme_support('post-thumbnails');
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -5,6 +5,31 @@
             <h2 class="layout__title">Introduction</h2>
             <p class="layout__txt">Bienvenue sur mon super site.</p>
         </section>
+
+        <section class="layout__latest latest">
+            <h2 class="latest__title">Nos dernières nouvelles</h2>
+            <div class="latest__container">
+                <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                <article class="post">
+                    <a href="<?= get_the_permalink(); ?>" class="post__link">Lire l'article "<?= get_the_title(); ?>"</a>
+                    <div class="post__card">
+                        <header class="post__head">
+                            <h3 class="post__title"><?= get_the_title(); ?></h3>
+                            <p class="post__meta">Publié par <?= get_the_author(); ?> le <time class="post__date" datetime="<?= get_the_date('c'); ?>"><?= get_the_date(); ?></time></p>
+                        </header>
+                        <figure class="post__fig">
+                            <?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'post__thumb', 'id' => 'test']); ?>
+                        </figure>
+                        <div class="post__excerpt">
+                            <p><?= get_the_excerpt(); ?></p>
+                        </div>
+                    </div>
+                </article>
+                <?php endwhile; else: ?>
+                <!-- Un message qui signale qu'il n'y a pas d'articles -->
+                <?php endif; ?>
+            </div>
+        </section>
     </main>
 
 <?php get_footer(); ?>
