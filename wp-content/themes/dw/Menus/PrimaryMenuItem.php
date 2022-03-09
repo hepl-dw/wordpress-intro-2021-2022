38b92ec97dd471d60b4a7a6e0bce51f954f1f030
Toon Van den Bos <toon@whitecube.be>
2022-03-09 16:41:48 +0100

    Added menu BEM classes generator

--- wp-content/themes/dw/Menus/PrimaryMenuItem.php
+++ wp-content/themes/dw/Menus/PrimaryMenuItem.php
@@ -42,4 +42,35 @@ class PrimaryMenuItem
     {
         $this->subitems[] = $instance;
     }
+
+    public function getBemClasses($base)
+    {
+        $icon = get_field('icon', $this->post);
+        $modifiers = [];
+
+        if($this->isCurrent()) {
+            $modifiers[] = 'current';
+        }
+
+        if($this->post->type === 'custom') {
+            $modifiers[] = 'url';
+        }
+
+        if($icon) {
+            $modifiers[] = $icon;
+        }
+
+        $value = $base;
+
+        foreach($modifiers as $modifier) {
+            $value .= ' ' . $base . '--' . $modifier;
+        }
+
+        return $value;
+    }
+
+    public function isCurrent()
+    {
+        return ($_SERVER['REQUEST_URI'] == parse_url($this->post->url, PHP_URL_PATH));
+    }
 }
--- wp-content/themes/dw/header.php
+++ wp-content/themes/dw/header.php
@@ -22,12 +22,12 @@
 
             <ul class="nav__container">
                 <?php foreach(dw_get_menu_items('primary') as $link): ?>
-                <li class="nav__item">
+                <li class="<?= $link->getBemClasses('nav__item'); ?>">
                     <a href="<?= $link->url; ?>" class="nav__link"><?= $link->label; ?></a>
                     <?php if($link->hasSubItems()): ?>
                     <ul class="nav__subitems">
                         <?php foreach($link->subitems as $sub): ?>
-                        <li class="nav__item">
+                        <li class="<?= $link->getBemClasses('nav__subitem'); ?>">
                             <a href="<?= $sub->url; ?>" class="nav__link"><?= $sub->label; ?></a>
                         </li>
                         <?php endforeach; ?>
