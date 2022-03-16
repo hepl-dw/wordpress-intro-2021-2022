cae4ead2bb4a26ef9513dbe2ab3efca219b289ed
Toon Van den Bos <toon@whitecube.be>
2022-03-16 15:17:54 +0100

    Added custom contact form

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -24,6 +24,23 @@ register_post_type('trip', [
     'rewrite' => ['slug' => 'voyages'],
 ]);
 
+register_post_type('message', [
+    'label' => 'Messages de contact',
+    'labels' => [
+        'name' => 'Messages de contact',
+        'singular_name' => 'Message de contact',
+    ],
+    'description' => 'Les messages envoyés par les utilisateurs via le formulaire de contact.',
+    'public' => false,
+    'show_ui' => true,
+    'menu_position' => 15,
+    'menu_icon' => 'dashicons-buddicons-pm',
+    'capabilities' => [
+        'create_posts' => false,
+    ],
+    'map_meta_cap' => true,
+]);
+
 // Récupérer les trips via une requête Wordpress
 function dw_get_trips($count = 20)
 {
@@ -81,6 +98,52 @@ function dw_get_menu_items($location)
 
     // Retourner les éléments de menu de niveau 0
     return $items;
+}
+
+// Enregistrer le traitement du formulaire de contact personnalisé.
+
+add_action('admin_post_submit_contact_form', 'dw_handle_submit_contact_form');
+
+function dw_handle_submit_contact_form()
+{
+    $nonce = $_POST['_wpnonce'];
+
+    if(wp_verify_nonce($nonce, 'nonce_check_contact_form')) {
+        $firstname = sanitize_text_field($_POST['firstname']);
+        $lastname = sanitize_text_field($_POST['lastname']);
+        $email = sanitize_email($_POST['email']);
+        $phone = sanitize_text_field($_POST['phone']);
+        $message = sanitize_text_field($_POST['message']);
+
+        if($firstname && $lastname && $email && $message) {
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                if(($_POST['rules'] ?? null) === '1') {
+                    // Stocker en base de données
+                    $id = wp_insert_post([
+                        'post_type' => 'message',
+                        'post_title' => 'Message de ' . $firstname . ' ' . $lastname,
+                        'post_content' => $message,
+                        'post_status' => 'publish',
+                    ]);
+
+                    // Envoyer un mail
+                    $content = 'Bonjour, un nouveau message de contact a été envoyé.<br />';
+                    $content .= 'Pour le visualiser : ' . get_edit_post_link($id);
+
+                    wp_mail('toon@whitecube.be', 'Nouveau message', $content);
+                } else {
+                    // TODO : afficher erreur de validation "conditions générales"
+                }
+            } else {
+                // TODO : afficher erreur de validation de type "email incorrect"
+            }
+        } else {
+            // TODO : afficher erreurs de validation de type "required"
+        }
+    } else {
+        // C'est pas OK.
+        // TODO : afficher un message d'erreur "unauthorized"
+    }
 }
 
 
